952bf69705baf56c4468bc3319f6058635f4f1b6
max.breslin <max.breslin@pcc.edu>
2022-11-03 11:08:44 -0700

    kind of works

--- practice-midterm/dlist.cpp
+++ practice-midterm/dlist.cpp
@@ -15,13 +15,26 @@ void insert(node * & head, int newInt, int position) {
     if (position == 0) {
         node * temp = new node;
         temp->data = newInt;
-        head->previous->next = temp;
-        temp->previous = head->previous;
+
+        temp->previous = nullptr;
         temp->next = head;
         head->previous = temp;
+        head = temp;
         return;
     }
-    return insert(head->next, newInt, position - 1);
+    if (position == 1) {
+        node * temp = new node;
+        temp->data = newInt;
+
+        temp->previous = head;
+        temp->next = head->next;
+        
+        head->next->previous = temp;
+        head->next = temp;
+
+        return;
+    }
+    insert(head->next, newInt, position - 1);
 }
 
 int remove(node * & head, int position) {
@@ -29,16 +42,23 @@ int remove(node * & head, int position) {
         return -1;
     }
     if (position == 0) {
+        node * temp = head;
         int data = head->data;
 
-        if (head->previous) {
-            head->previous->next = head->next;
-        }
-        if (head->next) {
-            head->next->previous = head->previous;
-        }
+        head->next->previous = nullptr;
+        head = head->next;
 
-        delete head;
+        delete temp;
+        return data;
+    }
+    if (position == 1) {
+        node * temp = head->next;
+        int data = temp->data;
+
+        head->next = temp->next;
+        temp->next->previous = head;
+
+        delete temp;
         return data;
     }
     return remove(head->next, position - 1);
--- practice-midterm/main.cpp
+++ practice-midterm/main.cpp
@@ -10,9 +10,18 @@ int main()
     cout << "Item count: " << count(head) << endl;
     cout << "Inserting 5 at position 3..." << endl;
     insert(head, 5 , 3);
-    // cout << "Integer at position 3 removed: " << remove(head, 0) << endl;
-
-
+    cout << "List after insertion:" << endl;
+    display(head);
+    cout << "Inserting 10 at position 0..." << endl;
+    insert(head, 10 , 0);
+    cout << "List after insertion:" << endl;
+    display(head);
+    cout << "Integer at position 1 removed: " << remove(head, 1) << endl;
+    cout << "List after removal:" << endl;
+    display(head);
+    cout << "Integer at position 0 removed: " << remove(head, 0) << endl;
+    cout << "List after removal:" << endl;
+    display(head);
 
     display(head);
     destroy(head);
